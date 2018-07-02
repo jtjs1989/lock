99e7e5ea07a3b4dcb4cb9ecc744781d0023b45ab
Administrator <Administrator@WIN-L6GEU52E9PH>
2018-07-02 23:31:48 +0800

    commit

--- src/main/java/com/cb/lock/StringRedisTemplateExt.java
+++ src/main/java/com/cb/lock/StringRedisTemplateExt.java
@@ -1,19 +1,33 @@
 package com.cb.lock;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Timer;
+import java.util.concurrent.TimeoutException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import redis.clients.jedis.exceptions.JedisException;
+
 @Component
-public class StringRedisTemplateExt extends StringRedisTemplate {
+public class StringRedisTemplateExt implements FactoryBean, InitializingBean {
 
 	private static final Logger logger = LoggerFactory.getLogger(StringRedisTemplateExt.class);
+	
+	private Object proxyObject;
+	
+	public StringRedisTemplateExt(StringRedisTemplate redisTemplate) {
+		this.proxyObject = redisTemplate;
+	}
+	
 	/**
 	 * redis断路器
 	 * true 代表执行远程Redis调用
@@ -52,6 +66,37 @@ public class StringRedisTemplateExt extends StringRedisTemplate {
 				}
 			}
 		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		proxyObject = Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[] {RedisOperations.class}, new InvocationHandler() {
+			
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				// TODO Auto-generated method stub
+				try {
+					
+					return method.invoke(proxy, args);
+				} catch (JedisException e) {
+					// TODO: handle exception
+					throw e;
+				} finally {
+					// TODO: handle finally clause
+				}
+			}
+		});
+	}
+
+	@Override
+	public Object getObject() throws Exception {
+		return proxyObject;
+	}
+
+	@Override
+	public Class getObjectType() {
+		return proxyObject.getClass();
 	}
 	
 }
