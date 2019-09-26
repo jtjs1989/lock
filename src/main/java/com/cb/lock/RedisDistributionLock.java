b7f2e9f7c4587f070e3b414a50823e4243f034ef
jtjs1989 <39582376+jtjs1989@users.noreply.github.com>
2019-09-26 16:13:00 +0800

    Update RedisDistributionLock.java
    优化锁逻辑，优先在本地获取JVM锁，获得JVM锁的线程再尝试获取redis锁标记，在高并发场景下能明显提高性能

--- src/main/java/com/cb/lock/RedisDistributionLock.java
+++ src/main/java/com/cb/lock/RedisDistributionLock.java
@@ -1,6 +1,9 @@
 package com.cb.lock;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -32,6 +35,8 @@ public class RedisDistributionLock implements Lock {
 
     private static final Logger log = LoggerFactory.getLogger(RedisDistributionLock.class);
 
+    private static final Map<String, Semaphore> jvm_lock_map = new ConcurrentHashMap<>();
+
     public static final String KeyPref = "lock_";
     //默认缓存时间 10秒
     private static final int defaultExpTime = 10 * 1000;
@@ -40,7 +45,11 @@ public class RedisDistributionLock implements Lock {
     private String value;
     private boolean isLock;
     /**
-     * lockKey过期时间
+     * 是否使用JVM本地同步锁
+     */
+    private boolean useJvmLock = true;
+    /**
+     * lock过期时间
      */
     private int expire;
     /**
@@ -54,15 +63,15 @@ public class RedisDistributionLock implements Lock {
 
     private static final int defaultThreadSleepTime = 5;
     /**
-     * 获取锁失败 线程 sleep 时间
+     * 获取锁失败的线程 sleep 时间
      */
     private long threadSleepTime;
     /**
      * 释放锁时删除锁的Lua脚本
      */
     private static final DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<Boolean>(
-            "if redis.call(\"get\",KEYS[1]) == ARGV[1] then \n" +
-                    "    redis.call(\"del\",KEYS[1]) \n" +
+    		"if redis.call(\"get\",KEYS[1]) == ARGV[1] then \n" +
+					"    redis.call(\"del\",KEYS[1]) \n" +
                     "	 return true \n" +
                     "else \n" +
                     "	return false \n" +
@@ -74,15 +83,17 @@ public class RedisDistributionLock implements Lock {
      * @param redisTemplate
      * @param expire        超时时间  单位为毫秒
      */
-    public RedisDistributionLock(String lockKey, StringRedisTemplate redisTemplate, int expire, int thradSleepTime) {
+    public RedisDistributionLock(String lockKey, StringRedisTemplate redisTemplate, int expire, int threadSleepTime, boolean useJvmLock) {
         this.redisTemplate = redisTemplate;
         this.lockKey = KeyPref + lockKey;
         this.expire = expire;
-        this.threadSleepTime = thradSleepTime;
+        this.threadSleepTime = threadSleepTime;
+        this.lockTimestamp = System.currentTimeMillis();
+        this.useJvmLock = useJvmLock;
     }
 
     public RedisDistributionLock(String lockKey, StringRedisTemplate redisTemplate, int expire) {
-        this(lockKey, redisTemplate, expire, defaultThreadSleepTime);
+        this(lockKey, redisTemplate, expire, defaultThreadSleepTime, true);
     }
 
     /**
@@ -104,31 +115,44 @@ public class RedisDistributionLock implements Lock {
      * @param threadSleepTime 线程休眠时间
      */
     public RedisDistributionLock(String lockKey, int exp, int threadSleepTime) {
-        this(lockKey, SpringContextUtil.getBean(StringRedisTemplate.class), exp, threadSleepTime);
+        this(lockKey, SpringContextUtil.getBean(StringRedisTemplate.class), exp, threadSleepTime, true);
     }
 
     @Override
     public void lock() {
-        internalLock(-1, null, Integer.MAX_VALUE);
+        if (useJvmLock) {
+            acquireLock(-1, null, Integer.MAX_VALUE);
+        } else {
+            internalLock(-1, null, Integer.MAX_VALUE);
+        }
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-        throw new InterruptedException();
+        lock();
     }
 
     @Override
     public boolean tryLock() {
-        return internalLock(-1, null, 1);
+        if (useJvmLock) {
+            return tryAcquireLock();
+        } else {
+            return internalLock(-1, null, 1);
+        }
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return internalLock(time, unit, Integer.MAX_VALUE);
+        if (useJvmLock) {
+            return acquireLock(time, unit, Integer.MAX_VALUE);
+        } else {
+            return internalLock(time, unit, Integer.MAX_VALUE);
+        }
     }
 
     /**
-     * 真正的加锁逻辑
+     * 真正的加锁逻辑,
+     * 支持优先本地获取锁，获取到本地锁到线程再去redis轮训获取分布式锁
      *
      * @param time  同步等待锁的时间
      * @param unit
@@ -145,28 +169,39 @@ public class RedisDistributionLock implements Lock {
             time = Integer.MAX_VALUE;
             unit = TimeUnit.MILLISECONDS;
         }
-        int lockTimes = 0;
-        while (lockTimestamp + unit.toMillis(time) > System.currentTimeMillis() && lockTimes < times) {
-            long timestamp = System.currentTimeMillis();
-            String redValue = String.valueOf(timestamp + expire);
-            boolean success = redisSetNx(lockKey, redValue, expire);
-            lockTimes++;
-            if (success) {
-                LockThreadLocalManager.firstLockSuccess(lockKey);
-                value = redValue;
-                lockedTimestamp = timestamp;
-                isLock = true;
-                if (log.isDebugEnabled()) {
-                    log.info("locked:{}, try_lock_times:{}", lockKey, times);
-                }
-                return true;
-            } else {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(threadSleepTime);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        boolean hasException = false;
+        try {
+            int lockTimes = 0;
+            while (lockTimestamp + unit.toMillis(time) > System.currentTimeMillis() && lockTimes < times) {
+                long timestamp = System.currentTimeMillis();
+                String redValue = String.valueOf(timestamp + expire);
+                boolean success = redisSetNx(lockKey, redValue, expire);
+                lockTimes++;
+                if (success) {
+                    LockThreadLocalManager.firstLockSuccess(lockKey);
+                    value = redValue;
+                    lockedTimestamp = timestamp;
+                    isLock = true;
+                    if (log.isDebugEnabled()) {
+                        log.info("Thread[{}] acquired lock[{}], try_lock_times:{}", Thread.currentThread().getId(), lockKey, lockTimes);
+                    }
+                    return true;
+                } else {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(threadSleepTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("lock exception",e);
+            hasException = true;
+        }
+        if (hasException) {
+            deleteKey();
+            throw new LockException("获取锁异常");
         }
         return false;
     }
@@ -177,12 +212,17 @@ public class RedisDistributionLock implements Lock {
     @Override
     public void unlock() {
         int state = LockThreadLocalManager.release(lockKey);
-        if (isLock && state == 0) {
+        if (state <= 0) {
             LockThreadLocalManager.clean(lockKey);
-            redisTemplate.execute(redisScript, Arrays.asList(lockKey), value);
-            if (log.isDebugEnabled()) {
-                log.info("unlock[{}], lockedTimes:{}ms", lockKey, (System.currentTimeMillis() - lockedTimestamp));
-            }
+            deleteKey();
+            release();
+        }
+    }
+
+    private void deleteKey() {
+        redisTemplate.execute(redisScript, Arrays.asList(lockKey), value);
+        if (log.isDebugEnabled()) {
+            log.info("unlock[{}], lockedTimes:{}ms", lockKey, (System.currentTimeMillis() - lockedTimestamp));
         }
     }
 
@@ -213,5 +253,67 @@ public class RedisDistributionLock implements Lock {
         } finally {
             RedisConnectionUtils.releaseConnection(connection, redisTemplate.getConnectionFactory());
         }
+    }
+
+    /**
+     * 阻塞获取锁
+     * 首先获取JVM本地锁，获取成功再去获取Redis分布式锁
+     * @param time
+     * @param unit
+     * @param times
+     * @throws InterruptedException
+     * @throws LockException
+     */
+    private boolean acquireLock(long time, TimeUnit unit, final int times) throws LockException {
+        Semaphore semaphore = getSemaphore();
+        try {
+            semaphore.acquire();
+            return internalLock(time, unit, times);
+        } catch (LockException e) {
+            semaphore.release();
+            throw e;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("",e);
+        }
+        return false;
+    }
+
+    /**
+     * 尝试一次获取锁
+     * @return
+     */
+    private boolean tryAcquireLock() {
+        Semaphore semaphore = getSemaphore();
+        if (semaphore.tryAcquire()) {
+            try {
+                if (internalLock(-1, null, 1)) {
+                    return true;
+                } else {
+                    semaphore.release();
+                }
+            } catch (LockException e) {
+                semaphore.release();
+            }
+        }
+        return false;
+    }
+
+    private void release() {
+        getSemaphore().release();
+    }
+
+    private Semaphore getSemaphore() {
+        Semaphore semaphore = jvm_lock_map.get(lockKey);
+        if (semaphore == null) {
+            semaphore = new Semaphore(1);
+            jvm_lock_map.putIfAbsent(lockKey, semaphore);
+            semaphore = jvm_lock_map.get(lockKey);
+        }
+        return semaphore;
+    }
+
+    public void setUseJvmLock(boolean useJvmLock) {
+        this.useJvmLock = useJvmLock;
     }
 }
